2e49310b1cbbab1e4c5b9a0d8c3cfb9f978a1565
naturalnaveenkumar <98877780+naturalnaveenkumar@users.noreply.github.com>
2022-07-12 13:16:20 +0530

    Update PayPalButton.js

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -1,1 +1,49 @@
 
+import React from "react";
+import PaypalExpressBtn from "react-paypal-express-checkout";
+
+export default class MyApp extends React.Component {
+  render() {
+    const onSuccess = payment => {
+     
+      console.log("The payment was succeeded!", payment);
+      this.props.clearCart();
+      this.props.history.push("/");
+     
+    };
+
+    const onCancel = data => {
+      // User pressed "cancel" or close Paypal's popup!
+      console.log("The payment was cancelled!", data);
+    
+    };
+
+    const onError = err => {
+      
+      console.log("Error!", err);
+   
+    };
+
+    let env = "sandbox";
+    let currency = "USD"; /
+    //let total = 1;
+    
+
+    const client = {
+      sandbox: process.env.REACT_APP_APP_ID,
+      production: "YOUR-PRODUCTION-APP-ID"
+    };
+    
+    return (
+      <PaypalExpressBtn
+        env={env}
+        client={client}
+        currency={currency}
+        total={this.props.totalAmount}
+        onError={onError}
+        onSuccess={onSuccess}
+        onCancel={onCancel}
+      />
+    );
+  }
+}
